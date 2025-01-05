9b67e4d886610ec3596af6842fcd778c1442317f
Purnachandra <pratap.careers@gmail.com>
2025-01-05 21:49:03 +0530

    Added Scrolling functionality and removed buttons

--- src/components/reel/Reel.jsx
+++ src/components/reel/Reel.jsx
@@ -1,43 +1,123 @@
 import { useEffect, useRef, useState } from 'react';
+import debounce from 'lodash.debounce';
 import style from './Reel.module.css';
-function Reel({ videos, handelCurrent, selected }) {
-  const vid = useRef();
-  const [current, setCurrent] = useState(selected);
-  const [liked, setLiked] = useState(
-    JSON.parse(localStorage.getItem('liked') || '[]').includes(current)
-  );
 
-  //   useEffect(() => {
-  //     window.onscroll = function (e) {
-  //       // print "false" if direction is down and "true" if up
-  //       const isUp = this.oldScroll > this.scrollY;
-  //       console.log(isUp, this.oldScroll - this.scrollY);
-  //       //   setCurrent(current + (isUp ? 1 : -1));
-  //       this.oldScroll = this.scrollY;
-  //     };
-  //     return () => (window.onscroll = () => {});
+function Reel({ videos, handelCurrent, selected }) {
+  const [current, setCurrent] = useState(0);
+  // const [scrollPosition, setScrollPosition] = useState(0);
+  const posRef = useRef(0);
+  const scrolled = useRef(false);
+  const vids = useRef([]);
+  const reelsRef = useRef();
+
+  const pre = e => {
+    e && e.stopPropagation();
+    if (current !== 0) setCurrent(c => c - 1);
+  };
+  const next = e => {
+    e && e.stopPropagation();
+    if (current !== videos.length - 1) setCurrent(c => c + 1);
+  };
+
+  function setRef(el, index) {
+    vids.current[index] = el;
+  }
+
+  useEffect(() => {
+    const handleScroll = debounce(() => {
+      if (reelsRef.current) {
+        console.log('Scroll', current);
+        const scrollPos = reelsRef.current.scrollTop;
+        const scrollPosition = posRef.current;
+        console.log(scrollPosition, scrollPos, current);
+        if (scrollPos > scrollPosition) next();
+        if (scrollPos < scrollPosition) pre();
+        posRef.current = scrollPos;
+      }
+    }, 100);
+    const el = reelsRef.current;
+    el.addEventListener('scroll', handleScroll);
+    return () => {
+      el.removeEventListener('scroll', handleScroll);
+    };
+  }, [posRef.current]);
+
+  //useEffect(() => {
+  //   console.log('Loading ', current);
+  //   vids.current.forEach((c, i) => {
+  //     const el = c.current;
+  //     if (i === current) {
+  //       console.log('Playing', current);
+  //       el.play();
+  //       el.scrollIntoView();
+  //     } else el.pause();
   //   });
+  // }, []);
+
+  useEffect(() => {
+    vids.current.forEach((c, i) => {
+      const el = c.current;
+      if (i === current) {
+        el.play();
+      } else el.pause();
+    });
+  }, [current]);
+
+  return (
+    <div
+      id='reels'
+      className={style.reel}
+      onClick={e => handelCurrent(e, null)}
+    >
+      {/* <img
+        onClick={pre}
+        className={style.btn}
+        src='./images/pre.png'
+        alt='pre'
+        width='40px'
+      /> */}
+
+      <div className={style.vids} ref={reelsRef}>
+        {videos.map((video, i) => (
+          <Video
+            video={video}
+            setRef={setRef}
+            current={current}
+            key={i}
+            index={i}
+          />
+        ))}
+      </div>
+
+      {/* <img
+        className={style.btn}
+        onClick={next}
+        src='./images/next.png'
+        alt='next'
+        width='40px'
+      /> */}
+    </div>
+  );
+}
+
+function Video({ video, current, index, setRef }) {
+  const vid = useRef();
+  const [liked, setLiked] = useState(false);
+  useEffect(() => {
+    setLiked(
+      JSON.parse(localStorage.getItem('liked') || '[]').includes(current)
+    );
+    setRef(vid, index);
+  });
 
   function pause(e) {
     e.stopPropagation();
     if (vid.current.paused) vid.current.play();
     else vid.current.pause();
   }
-
-  const pre = e => {
-    e.stopPropagation();
-    if (current !== 0) setCurrent(c => c - 1);
-  };
-
-  const next = e => {
-    e.stopPropagation();
-    if (current !== videos.length - 1) setCurrent(c => c + 1);
-  };
-
   function like(e) {
     e.stopPropagation();
     let likedVids = JSON.parse(localStorage.getItem('liked') || '[]');
-    console.log(likedVids);
     if (liked) {
       likedVids = likedVids.filter(e => e !== current);
       localStorage.setItem('liked', JSON.stringify(likedVids));
@@ -48,42 +128,30 @@ function Reel({ videos, handelCurrent, selected }) {
     setLiked(l => !l);
   }
 
-  //   let isLiked = likedVids.includes(current);
-  //   console.log(isLiked);
   return (
-    <div
-      id='reels'
-      className={style.reel}
-      onClick={e => handelCurrent(e, null)}
-    >
-      <img onClick={pre} src='./images/pre.png' alt='pre' width='40px' />
-
-      <div className={style.videoCnt}>
+    <div className={style.videoCnt}>
+      <div className={style.vidText}>
         <video
           ref={vid}
           className={style.video}
-          src={`./videos/${videos[current]}`}
+          src={`./videos/${video}`}
           onClick={pause}
           loop
-          autoPlay
+          autoPlay={false}
         />
         <div className={style.text}>
           <div className={style.title}>
-            <h1>Video {current + 1}</h1>
+            <h1>Video {index + 1}</h1>
             <div>
               <img
                 className={style.like}
                 onClick={like}
                 src={liked ? './images/liked.png' : './images/like.png'}
               />
-              <a
-                href={`./videos/${videos[current]}`}
-                download={`${current}.mp4`}
-              >
+              <a href={`./videos/${video}`} download={`${current}.mp4`}>
                 <img className={style.like} src='./images/down.png' />
               </a>
             </div>
-            {console.log(liked)}
           </div>
           <p>
             "Some old-fashioned things like fresh air and sunshine are hard to
@@ -91,8 +159,6 @@ function Reel({ videos, handelCurrent, selected }) {
           </p>
         </div>
       </div>
-
-      <img onClick={next} src='./images/next.png' alt='next' width='40px' />
     </div>
   );
 }
--- src/pages/reels/Reels.jsx
+++ src/pages/reels/Reels.jsx
@@ -1,44 +1,43 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import style from './Reels.module.css';
 import Reel from '../../components/reel/Reel';
 
 function Reels() {
   const reels = [
+    '0.webm',
     '1.webm',
     '2.webm',
     '3.webm',
     '4.webm',
-    '5.webm',
+    '0.webm',
     '1.webm',
     '2.webm',
     '3.webm',
     '4.webm',
-    '5.webm',
   ];
   const [current, setCurrent] = useState(null);
   const handelCurrent = (e, num) => setCurrent(c => (num != null ? num : null));
-
-  console.log(current);
   return (
     <>
-      {current !== null && (
+      {current !== null ? (
         <Reel videos={reels} handelCurrent={handelCurrent} selected={current} />
+      ) : (
+        <div className={style.reels}>
+          <div className={style.header}>
+            <h1>Nature's Reels</h1>
+          </div>
+          <div className={style.posters}>
+            {reels.map((c, i) => (
+              <video
+                className={style.poster}
+                src={`./videos/${c}`}
+                key={i}
+                onClick={e => handelCurrent(e, i)}
+              />
+            ))}
+          </div>
+        </div>
       )}
-      <div className={style.reels}>
-        <div className={style.header}>
-          <h1>Nature's Reels</h1>
-        </div>
-        <div className={style.posters}>
-          {reels.map((c, i) => (
-            <video
-              className={style.poster}
-              src={`./videos/${c}`}
-              key={i}
-              onClick={e => handelCurrent(e, i % 5)}
-            />
-          ))}
-        </div>
-      </div>
     </>
   );
 }
